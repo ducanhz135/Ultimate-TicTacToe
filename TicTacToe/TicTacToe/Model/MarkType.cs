f42792475ab7c6485e88b592ff7315b6da61d3ac
duc anh <ducanhz135@gmail.com>
2020-04-10 21:53:07 +0700

    commit

---
+++ TicTacToe/TicTacToe/Model/MarkType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Model
+{
+    public enum MarkType
+    {
+
+        /// <summary>
+        /// No Tick
+        /// </summary>
+        Free,
+        /// <summary>
+        /// Tick O
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// Tick X
+        /// </summary>
+        Cross
+
+    }
+}
